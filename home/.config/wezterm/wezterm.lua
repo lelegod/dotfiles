a2f3e3ea25ec6d80ca4bc8a58fb54d3d50ec41b2
lelegod <2021m622@kuas.ac.jp>
2026-09-06 21:18:28 +0200

    wezterm: copy the full pane scrollback with cmd+shift+a

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -10,4 +10,17 @@ config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 
+config.keys = {
+	-- Copy the whole pane, scrollback included (wezterm only copies selections by default)
+	{
+		key = "a",
+		mods = "CMD|SHIFT",
+		action = wezterm.action_callback(function(window, pane)
+			local text = pane:get_lines_as_text(pane:get_dimensions().scrollback_rows)
+			window:copy_to_clipboard(text)
+			window:toast_notification("wezterm", ("copied %d chars"):format(#text), nil, 2000)
+		end),
+	},
+}
+
 return config
